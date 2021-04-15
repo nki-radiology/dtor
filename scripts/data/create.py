8a139943843dee061982cbfafc0663b2fcde6bf5
sb <sean.benson@cern.ch>
2021-04-15 15:10:03 +0200

    remove hardcoding in nominal case

--- scripts/data/create.py
+++ scripts/data/create.py
@@ -1,4 +1,4 @@
 from dtor.datasets.dataset_nominal import CTImageDataset
 
 dset = CTImageDataset("data/external/Label.csv", label="L_LTP_date",
-                      shape=(112, 112, 16), stride=(56, 56, 8))  # shape=(64,64,48), stride=(32, 32, 24))
+                      shape=(64, 64, 64), stride=(32, 32, 32))  # shape=(64,64,48), stride=(32, 32, 24))
