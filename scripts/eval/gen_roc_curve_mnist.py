5065f8aafd376bc2310b59d78d3e3ecf3f0913a5
Marjaneh <39339809+Marjaneh-T@users.noreply.github.com>
2021-05-28 14:16:08 +0200

    Update gen_roc_curve_mnist.py

--- scripts/eval/gen_roc_curve_mnist.py
+++ scripts/eval/gen_roc_curve_mnist.py
@@ -14,8 +14,8 @@ set_plt_config()
 
 # Load test data
 data = MNIST3DDataset(h5_file="data/external/mnist/full_dataset_vectors.h5", tr_test="test")
-tot_folds = 1
-prefix = "mnist_example"
+tot_folds = 3
+prefix = "default"
 
 # Process folds
 res_preds = []
@@ -23,18 +23,24 @@ y_preds = []
 y_labels = []
 for f in range(tot_folds):
     # Get model for the fold
-    model = load_model(prefix, f, model_type="nominal_mnist")
-
+    _f, _, _ = data[0]
+    sample = _f.unsqueeze(0)
+    use_cuda = torch.cuda.is_available()   
+    device = torch.device("cuda" if use_cuda else "cpu")
+    sample = sample.to(device)
+    full_name=os.path.join("/home/marjaneh/ltp-prediction/results",  'model-' + str(prefix) +'-fold'+ str(f) +'.pth') 
+    model = load_model(prefix, f, model_type="resnet18+dense",full_name=full_name, sample=sample)
+    model = model.to(device)
     # Generate vector of predictions and true labels
     y_preds = []
     y_labels = []
 
     for n in range(400):
         f, truth, _ = data[n]
-        x = torch.from_numpy(f).unsqueeze(0)
+        x = f.unsqueeze(0)
+        x = x.to(device)
         l, p = model(x)
-
-        pred = p[0][1].detach().numpy()
+        pred = p[0][1].detach().cpu()  #.numpy()
 
         y_preds.append(pred)
         y_labels.append(truth)
