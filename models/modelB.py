6dc891040729dcb801df5c747a5dedcce18cfcca
Sean Benson <s.benson@nki.nl>
2021-06-03 16:44:13 +0200

    minor updates

--- models/modelB.py
+++ models/modelB.py
@@ -3,13 +3,17 @@ import torch.nn as nn
 
 
 class ModelB(nn.Module):
-    def __init__(self, inmodel, base_output_shape=3172, output_classes=2, fix_inmodel=True):
+    def __init__(self, inmodel, base_output_shape=3172, output_classes=2, fix_inmodel=14):
         super(ModelB, self).__init__()
         self.modelA = inmodel
 
         if fix_inmodel:
-            for param in self.modelA.parameters():
-                param.requires_grad = False
+            assert isinstance(fix_inmodel,int), "Tell me how many layers to fix"
+            for n, l in enumerate(self.modelA.children()):
+                if n<fix_inmodel:
+                    continue
+                for param in l.parameters():
+                    param.requires_grad = False
 
         self.classifier = nn.Linear(base_output_shape, output_classes)
         self.head_softmax = nn.Softmax(dim=1)
